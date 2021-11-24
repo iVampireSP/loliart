71f4a5b7314849c6197bf8a78ee8b65b7d0a6ae6
iVampireSP.com <im@ivampiresp.com>
2021-11-24 22:34:06 +0800

    虚拟机 模块初始化

---
+++ Modules/VirtualMachine/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('virtualmachine::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('virtualmachine.name') !!}
+    </p>
+@endsection
