8bfcdbe91274b7c90d166dc70201dd27d81d9305
iVampireSP.com <im@ivampiresp.com>
2021-11-24 22:33:54 +0800

    测试组件

--- Modules/Pterodactyl/Resources/views/index.blade.php
+++ Modules/Pterodactyl/Resources/views/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <h1>Hello World</h1>
-
+    <livewire:pterodactyl::counter />
     <p>
         This view is loaded from module: {!! config('pterodactyl.name') !!}
     </p>
