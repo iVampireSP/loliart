95b0ad78694d2e0c4250de97ad37cfe55ae5c095
iVampireSP.com <im@ivampiresp.com>
2021-11-24 21:48:40 +0800

    移除 pterodactyl 模块

--- Modules/PterodactylPanel/Routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-// Route::get('/ptero', 'PterodactylPanelController@index');
-
-Route::domain('pterodactyl.loliart.test')->prefix('/')->name('ptero.')->group(function () {
-    Route::get('/', 'PterodactylPanelController@index')->name('index');
-});
