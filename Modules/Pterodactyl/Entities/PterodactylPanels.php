cd3d878efa3c126411525dfed8016455d53a16cd
iVampireSP.com <im@ivampiresp.com>
2021-11-25 09:59:38 +0800

    翼龙 面板 模型

---
+++ Modules/Pterodactyl/Entities/PterodactylPanels.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\Pterodactyl\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class PterodactylPanels extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [];
+}
