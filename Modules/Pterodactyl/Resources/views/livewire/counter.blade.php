dce1ff379f782f2d1f6ff18341a97de4959fc624
iVampireSP.com <im@ivampiresp.com>
2021-11-25 09:59:45 +0800

    移除 测试

--- Modules/Pterodactyl/Resources/views/livewire/counter.blade.php
+++
@@ -1,5 +0,0 @@
-<div>
-    <h3>The <code>Counter</code> livewire component is loaded from the <code>Pterodactyl</code>
-        module.</h3>
-    <span wire:click='increment'> {{ $count }} </span>
-</div>
