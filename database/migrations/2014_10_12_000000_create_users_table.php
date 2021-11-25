106fde751ea1f00ae96f3c2c83c5ea72dc9f5954
iVampireSP.com <im@ivampiresp.com>
2021-11-25 19:09:49 +0800

    增加 上次登录

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->timestamp('last_login_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
