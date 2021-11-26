fed5d5c59352754b139d0bc39158fb8987cb0ea8
iVampireSP.com <im@ivampiresp.com>
2021-11-26 08:03:47 +0800

    移除 LightAppEngine 模组

--- Modules/LightAppEngine/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('lightappengine::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('lightappengine.name') !!}
-    </p>
-@endsection
