d8b5e8d02b545d424d87e1fd1df00c2cdf9ff74a
iVampireSP.com <im@ivampiresp.com>
2021-11-26 08:10:12 +0800

    移除 Module

--- Modules/Pterodactyl/Resources/views/index.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <livewire:pterodactyl::create />
-    <p>
-        This view is loaded from module: {!! config('pterodactyl.name') !!}
-    </p>
-@endsection
--- Modules/Pterodactyl/Routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::domain('pterodactyl.' . config('app.domain'))->prefix('/')->as('www.')->group(function () {
-    Route::get('/', 'PterodactylController@index');
-});
