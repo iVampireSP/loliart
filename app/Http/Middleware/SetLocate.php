6830e9049f0870062e32f9c24b80fedf061e0104
iVampireSP.com <im@ivampiresp.com>
2021-11-27 20:49:38 +0800

    翻译中间件

---
+++ app/Http/Middleware/SetLocate.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Jenssegers\Agent\Facades\Agent;
+
+class SetLocate
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $languages = Agent::languages();
+        $languages = $languages;
+        // App::setLocale($languages);
+        if (session()->get('locate') != $languages) {
+            session()->forget('locate');
+        }
+        session()->put('locate', $languages);
+        return $next($request);
+    }
+}
