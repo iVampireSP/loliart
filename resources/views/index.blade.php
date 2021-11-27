da3d96e628269f980303c863e53514cb969fc159
iVampireSP.com <im@ivampiresp.com>
2021-11-27 22:51:08 +0800

    自动翻译

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,75 +1,9 @@
 @extends('layouts.app')
 
+@section('title', tr('Home page'))
+
 @section('content')
 
-    <div class="mdui-typo-display-2">很棒的</div>
-    <div class="mdui-typo-display-1">Worktable</div>
-    <div class="mdui-typo-display-2">很棒的</div>
-    <div class="mdui-typo-display-1">Worktable</div>
-    <div class="mdui-typo-display-2">很棒的</div>
-    <div class="mdui-typo-display-1">Worktable</div>
-    <div class="mdui-typo-display-2">很棒的</div>
-    <div class="mdui-typo-display-1">Worktable</div>
-    <div class="mdui-typo-display-2">很棒的</div>
-    <div class="mdui-typo-display-1">Worktable</div>
-    <div class="mdui-typo-display-2">很棒的</div>
-    <div class="mdui-typo-display-1">Worktable</div>
-    <div class="mdui-typo-display-2">很棒的</div>
-    <div class="mdui-typo-display-1">Worktable</div>
-    <div class="mdui-typo-display-2">很棒的</div>
-    <div class="mdui-typo-display-1">Worktable</div>
-    <div class="mdui-typo-display-2">很棒的</div>
-    <div class="mdui-typo-display-1">Worktable</div>
-    <div class="mdui-typo-display-2">很棒的</div>
-    <div class="mdui-typo-display-1">Worktable</div>
-    <div class="mdui-typo-display-2">很棒的</div>
-    <div class="mdui-typo-display-1">Worktable</div>
-    <div class="mdui-typo-display-2">很棒的</div>
-    <div class="mdui-typo-display-1">Worktable</div>
-    <div class="mdui-typo-display-2">很棒的</div>
-    <div class="mdui-typo-display-1">Worktable</div>
-    <div class="mdui-typo-display-2">很棒的</div>
-    <div class="mdui-typo-display-1">Worktable</div>
-    <div class="mdui-typo-display-2">很棒的</div>
-    <div class="mdui-typo-display-1">Worktable</div>
-    <div class="mdui-typo-display-2">很棒的</div>
-    <div class="mdui-typo-display-1">Worktable</div>
-    <div class="mdui-typo-display-2">很棒的</div>
-    <div class="mdui-typo-display-1">Worktable</div>
-    <div class="mdui-typo-display-2">很棒的</div>
-    <div class="mdui-typo-display-1">Worktable</div>
-    <div class="mdui-typo-display-2">很棒的</div>
-    <div class="mdui-typo-display-1">Worktable</div>
-    <div class="mdui-typo-display-2">很棒的</div>
-    <div class="mdui-typo-display-1">Worktable</div>
-    <div class="mdui-typo-display-2">很棒的</div>
-    <div class="mdui-typo-display-1">Worktable</div>
-    <div class="mdui-typo-display-2">很棒的</div>
-    <div class="mdui-typo-display-1">Worktable</div>
-    <div class="mdui-typo-display-2">很棒的</div>
-    <div class="mdui-typo-display-1">Worktable</div>
-    <div class="mdui-typo-display-2">很棒的</div>
-    <div class="mdui-typo-display-1">Worktable</div>
-    <div class="mdui-typo-display-2">很棒的</div>
-    <div class="mdui-typo-display-1">Worktable</div>
-    <div class="mdui-typo-display-2">很棒的</div>
-    <div class="mdui-typo-display-1">Worktable</div>
-    <div class="mdui-typo-display-2">很棒的</div>
-    <div class="mdui-typo-display-1">Worktable</div>
-    <div class="mdui-typo-display-2">很棒的</div>
-    <div class="mdui-typo-display-1">Worktable</div>
-    <div class="mdui-typo-display-2">很棒的</div>
-    <div class="mdui-typo-display-1">Worktable</div>
-    <div class="mdui-typo-display-2">很棒的</div>
-    <div class="mdui-typo-display-1">Worktable</div>
-    <div class="mdui-typo-display-2">很棒的</div>
-    <div class="mdui-typo-display-1">Worktable</div>
-    <div class="mdui-typo-display-2">很棒的</div>
-    <div class="mdui-typo-display-1">Worktable</div>
-    <div class="mdui-typo-display-2">很棒的</div>
-    <div class="mdui-typo-display-1">Worktable</div>
-    <div class="mdui-typo-display-2">很棒的</div>
-    <div class="mdui-typo-display-1">Worktable</div>
-
+    <div class="mdui-typo-display-1">{{ tr('Nice and powerful Worktable') }}</div>
 
 @endsection
--- resources/views/password/confirm.blade.php
+++ resources/views/password/confirm.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Confirm password')
+@section('title', tr('Confirm password'))
 
 @section('content')
     <div class="mdui-typo-display-2">{{ tr('Confirm your password.') }}</div>
@@ -8,7 +8,7 @@
     <form method="POST" action="{{ route('password.confirm_password') }}">
         @csrf
         <div class="mdui-textfield mdui-textfield-floating-label">
-            <label class="mdui-textfield-label">{{ tr('Your password.') }}</label>
+            <label class="mdui-textfield-label">{{ tr('Your password') }}</label>
             <input class="mdui-textfield-input" type="password" name="password" />
         </div>
 
