22ce32d57e1b0c644f78fa9dc42d13ee6f268520
iVampireSP.com <im@ivampiresp.com>
2021-11-27 20:58:29 +0800

    辅助函数

--- app/Helpers.php
+++ app/Helpers.php
@@ -3,3 +3,12 @@
 /**
  * Helpers
  */
+
+use App\Http\Controllers\TranslateController;
+
+if (!function_exists('tr')) {
+    function tr($str)
+    {
+        return TranslateController::translate($str);
+    }
+}
