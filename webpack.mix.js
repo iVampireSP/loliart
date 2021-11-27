e77f437e59715ee00e586156c2fe8d5dea2d3974
iVampireSP.com <im@ivampiresp.com>
2021-11-27 23:06:14 +0800

    移除 通知

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,3 +15,5 @@ mix.js('resources/js/app.js', 'public/js').js('resources/js/menu.js', 'public/js
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]).version();
+
+mix.disableNotifications();
