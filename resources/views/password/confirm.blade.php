46dbb92f05289a013243caf0611b237b8049ad88
iVampireSP.com <im@ivampiresp.com>
2021-11-27 21:47:25 +0800

    翻译字段

--- resources/views/password/confirm.blade.php
+++ resources/views/password/confirm.blade.php
@@ -3,16 +3,17 @@
 @section('title', 'Confirm password')
 
 @section('content')
-    <div class="mdui-typo-display-2">{{ __('Confirm your password.') }}</div>
+    <div class="mdui-typo-display-2">{{ tr('Confirm your password.') }}</div>
 
     <form method="POST" action="{{ route('password.confirm_password') }}">
         @csrf
         <div class="mdui-textfield mdui-textfield-floating-label">
-            <label class="mdui-textfield-label">{{ __('Your password.') }}</label>
+            <label class="mdui-textfield-label">{{ tr('Your password.') }}</label>
             <input class="mdui-textfield-input" type="password" name="password" />
         </div>
 
-        <button type="submit" class="mdui-btn mdui-btn-raised mdui-ripple mdui-color-theme-accent">Next</button>
+        <button type="submit"
+            class="mdui-btn mdui-btn-raised mdui-ripple mdui-color-theme-accent">{{ tr('Next') }}</button>
     </form>
 
 
--- resources/views/password/reset.blade.php
+++ resources/views/password/reset.blade.php
@@ -3,16 +3,17 @@
 @section('title', 'Password')
 
 @section('content')
-    <div class="mdui-typo-display-2">{{ __('Setting up your password.') }}</div>
+    <div class="mdui-typo-display-2">{{ tr('Setting up your password.') }}</div>
 
     <form method="POST" action="{{ route('password.setup_password') }}">
         @csrf
         <div class="mdui-textfield mdui-textfield-floating-label">
-            <label class="mdui-textfield-label">{{ __('Your new password.') }}</label>
+            <label class="mdui-textfield-label">{{ tr('Your new password.') }}</label>
             <input class="mdui-textfield-input" type="password" name="password" />
         </div>
 
-        <button type="submit" class="mdui-btn mdui-btn-raised mdui-ripple mdui-color-theme-accent">Next</button>
+        <button type="submit"
+            class="mdui-btn mdui-btn-raised mdui-ripple mdui-color-theme-accent">{{ tr('Next') }}</button>
     </form>
 
 
