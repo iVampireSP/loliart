249525ed764db69b560b00de2824e5773e485111
iVampireSP.com <im@ivampiresp.com>
2021-11-28 19:57:10 +0800

    移除 固定的频道

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,8 +6,3 @@ window.Echo = new Echo({
     broadcaster: 'socket.io',
     host: window.location.hostname + ':6001'
 });
-
-window.Echo.channel('workbench_database_test-event')
-    .listen('ExampleEvent', (e) => {
-        console.log(e);
-    });
