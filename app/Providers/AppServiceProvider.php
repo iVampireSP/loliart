e55fc4e7f17e734df8e9ec5c969c4645a8c5c08c
iVampireSP.com <im@ivampiresp.com>
2021-11-29 21:26:40 +0800

    SQL 日志

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // \DB::listen(function ($query) {
+        //     $tmp = str_replace('?', '"' . '%s' . '"', $query->sql);
+        //     $tmp = vsprintf($tmp, $query->bindings);
+        //     $tmp = str_replace("\\", "", $tmp);
+        //     \Log::info(' execution time: ' . $query->time . 'ms; ' . $tmp . "\n\n\t");
+        // });
     }
 }
