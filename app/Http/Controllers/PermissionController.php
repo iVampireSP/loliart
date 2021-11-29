4320466d0d749b8a0965fabe50851e3b8e767de3
iVampireSP.com <im@ivampiresp.com>
2021-11-29 18:54:40 +0800

    权限控制器

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -7,11 +7,10 @@ use Illuminate\Http\Request;
 use Spatie\Permission\Models\Role;
 use Illuminate\Support\Facades\Auth;
 use Spatie\Permission\Models\Permission;
-use App\Models\Permission as Permissions;
 
 class PermissionController extends Controller
 {
-    public function index(Permissions $permissions)
+    public function index()
     {
         // show all permissions
         // Permission::findOrCreate('admin.update');
