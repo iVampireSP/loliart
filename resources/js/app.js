1e34d7d8a280df3a2701675dc5a8df3b57249359
iVampireSP.com <im@ivampiresp.com>
2021-11-29 11:29:21 +0800

    移除 menu.js并更换实现方式

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,3 +6,27 @@ window.Echo = new Echo({
     broadcaster: 'socket.io',
     host: window.location.hostname + ':6001'
 });
+
+window.Progress = {
+    show: () => {
+        $('#top-progress').show()
+    },
+    hide: () => {
+        $('#top-progress').hide()
+    }
+}
+
+$(document).ajaxStart(function (event, xhr, options) {
+    Progress.show()
+});
+
+$(document).ajaxComplete(function (event, xhr, options) {
+    Progress.hide()
+});
+
+$.ajaxSetup({
+    global: true,
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+});
---
+++ resources/js/util.js
@@ -0,0 +1,57 @@
+window.util = {
+    event: {
+        listen: () => {
+            window.Echo.private(`user.${window.user.id}`)
+                .listen('UserEvent', (e) => {
+                    util.event.process(e)
+                });
+        },
+        process: (event) => {
+            switch (event.data.type) {
+                case 'team_invitation':
+                    ui.snackbar({
+                        message: 'You received a team invitation from ' + event.data.name,
+                        buttonText: 'View',
+                        onButtonClick: function () {
+                            ui.alert('button clicked');
+                        }
+                    });
+                    // ui.confirm(event.data.team_owner + ' invite you to join team: ' + event.data.name, () => {}, () => {})
+                    break;
+
+                default:
+                    break;
+            }
+            console.log(event);
+        }
+    },
+    menu: {
+        update: () => {
+            var url = window.location.protocol + '//' + window.location.host + window.location.pathname;
+            var length = $("#main-list a[href='" + url + "']").length;
+
+            if (!length) {
+                url = url.substr(0, url.length - 1);
+                length = $("#main-list a[href='" + url + "']").length;
+            }
+
+            if ($("#main-list a[href='" + url + "']").length > 0) {
+                $('#main-list .mdui-list-item').removeClass('mdui-list-item-active');
+                $("#main-list a[href='" + url + "']").addClass('mdui-list-item-active');
+            }
+        }
+    }
+}
+
+$(() => {
+    util.menu.update()
+    var title = document.title;
+    title = title.replace(' - ' + app_name, '');
+    $('#top-title').text(title);
+});
+
+if (window.history && window.history.pushState) {
+    window.onpopstate = function () {
+        util.menu.update();
+    };
+}
