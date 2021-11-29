64fa7029658abf1f5727696ee47824892bb5cfe6
iVampireSP.com <im@ivampiresp.com>
2021-11-29 12:35:54 +0800

    恢复 team

--- resources/js/util.js
+++ resources/js/util.js
@@ -1,4 +1,22 @@
 window.util = {
+    team: {
+        create: () => {
+            mdui.prompt('What is the name of the new team?',
+                function (value) {
+                    $.ajax({
+                        method: 'POST',
+                        url: route('teams.team.store'),
+                        data: {
+                            name: value,
+                        },
+                        success() {
+                            window.location.reload()
+                        }
+                    });
+                }
+            );
+        }
+    },
     event: {
         listen: () => {
             window.Echo.private(`user.${window.user.id}`)
