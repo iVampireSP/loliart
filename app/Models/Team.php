ba7d077224713ed63be7ce8438067ebaf2941791
iVampireSP.com <im@ivampiresp.com>
2021-11-29 19:47:27 +0800

    Super Admin

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -23,4 +23,20 @@ class Team extends Model
     {
         return $this->hasMany(TeamInvitation::class);
     }
+
+    public static function boot()
+    {
+        parent::boot();
+        // here assign this team to a global user with global default role
+        self::created(function ($model) {
+            // get session team_id for restore it later
+            $session_team_id = getPermissionsTeamId();
+            // set actual new team_id to package instance
+            setPermissionsTeamId($model);
+            // get the admin user and assign roles/permissions on new team model
+            User::find(auth()->id())->assignRole('Super Admin');
+            // restore session team_id to package instance
+            setPermissionsTeamId($session_team_id);
+        });
+    }
 }
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::after(function ($user, $ability) {
+            return $user->hasRole('Super Admin');
+        });
     }
 }
