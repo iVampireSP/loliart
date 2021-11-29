7aae9ecd87eeb25620c96b48b58d714ee2083f53
iVampireSP.com <im@ivampiresp.com>
2021-11-29 21:32:15 +0800

    团队创建

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -30,7 +30,7 @@ class Team extends Model
         // here assign this team to a global user with global default role
         self::created(function ($model) {
             // get session team_id for restore it later
-            $session_team_id = getPermissionsTeamId();
+            $session_team_id = getPermissionsTeamId($model);
             // set actual new team_id to package instance
             setPermissionsTeamId($model);
             // get the admin user and assign roles/permissions on new team model
