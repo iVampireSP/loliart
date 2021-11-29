550d845c6181f17f61af1a9d31ff109bed273bb4
iVampireSP.com <im@ivampiresp.com>
2021-11-29 18:45:45 +0800

    增加 节点

--- config/app.php
+++ config/app.php
@@ -55,6 +55,8 @@ return [
     'url' => env('APP_URL', 'http://localhost'),
     'domain' => env('APP_DOMAIN'),
 
+    'node' => env('NODE', 'standalone'),
+
     'asset_url' => env('ASSET_URL', null),
 
     /*
