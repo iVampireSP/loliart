42dff5a470322cdcaa2ea8b5e3e33e277d781c37
iVampireSP.com <im@ivampiresp.com>
2021-11-30 19:11:51 +0800

    更改对象名称

--- resources/views/teams/show.blade.php
+++ resources/views/teams/show.blade.php
@@ -3,22 +3,16 @@
 @section('title', tr('Team users of:') . $team->name)
 
 @section('content')
+    @can('Super Admin')
+        <div class="mdui-typo-headline-opacity">{{ tr('Super Admin') }}</div>
+    @endcan
     <div class="mdui-typo-display-2">{{ $team->name }}</div>
-    {{ session()->get('team_id') }}
-
-    {{-- @role('writer')
-        我是一个 writer!
-    @else
-        我不是一个 writer...
-    @endrole --}}
-
-    {{-- <a onclick="util.team.create()" class="mdui-btn mdui-color-theme-accent mdui-ripple">{{ tr('New Team') }}</a> --}}
-
-
+    <a onclick="util.url.to(route('teams.invitations'))"
+        class="mdui-btn mdui-color-theme-accent mdui-ripple">{{ tr('Invitations') }}</a>
     <div id="masonry" class="mdui-row">
         <ul class="mdui-list">
             @foreach ($team->users as $user)
-                {{ $user }}
+                {{ $user->name }}
             @endforeach
         </ul>
 
