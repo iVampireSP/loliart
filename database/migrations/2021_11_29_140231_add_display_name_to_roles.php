f7c09f340f0673e3c3124feee679009ffb9d1ad7
iVampireSP.com <im@ivampiresp.com>
2021-11-30 08:22:52 +0800

    增加 显示名称

---
+++ database/migrations/2021_11_29_140231_add_display_name_to_roles.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddDisplayNameToRoles extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('roles', function (Blueprint $table) {
+            $table->string('display_name')->index()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('roles', function (Blueprint $table) {
+            //
+        });
+    }
+}
