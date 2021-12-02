1e14f44dd99c21f325235367c1551149b8a2a0f3
iVampireSP.com <im@ivampiresp.com>
2021-12-02 11:10:09 +0800

    移除 VirtualMachine 模组

--- app/Modules/VirtualMachine/Routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::domain('virtualMachine.' . config('app.domain'))->prefix('/')->as('virtualMachine.')->group(function () {
-    Route::get('/', function () {
-        return view('index');
-    })->name('index');
-});
