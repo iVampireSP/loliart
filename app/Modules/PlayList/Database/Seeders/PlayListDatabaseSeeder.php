64b4928fa02a5a2e85175e0db08b2a3ca0071a11
iVampireSP.com <im@ivampiresp.com>
2021-12-02 19:22:35 +0800

    增加 PlayList Module

---
+++ app/Modules/PlayList/Database/Seeders/PlayListDatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Modules\PlayList\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class PlayListDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+        // $this->call("OthersTableSeeder");
+    }
+}
