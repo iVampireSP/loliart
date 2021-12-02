9646eb9b051a5529a2801ea2dfbc09c74db0bb2f
iVampireSP.com <im@ivampiresp.com>
2021-12-02 21:19:39 +0800

    增加测试

--- Modules/PlayList/Resources/views/index.blade.php
+++ Modules/PlayList/Resources/views/index.blade.php
@@ -1,10 +1,10 @@
-{{-- @extends('playlist::layouts.master') --}}
+@extends('layouts.app')
 
 @section('app-menu')
     @include('playlist::layouts.menu')
 @endsection
 
-@extends('layouts.app')
+@section('title', tr('Play list'))
 
 @section('content')
     <h1>Hello World</h1>
@@ -12,4 +12,6 @@
     <p>
         This view is loaded from module: {!! config('playlist.name') !!}
     </p>
+
+    <audio src="http://music.163.com/song/media/outer/url?id=317151.mp3" controls preload="auto"></audio>
 @endsection
