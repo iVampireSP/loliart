b75194302bca322c8efd52666077815ca57afd70
iVampireSP.com <im@ivampiresp.com>
2021-12-02 19:39:41 +0800

    独立出配置

--- config/oauth.php
+++ config/oauth.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'callback_uri' => 'https://login.loliart.test/callback',
-    'client_id' => '94f5e528-8b3b-4faa-abbf-9e7f02f4f169',
-    'client_secret' => 'XBH6jqO5T18HIzq9Qgh7ooZPpdQmeWfqCR2MOfju',
-    'oauth_auth_url' => 'https://oauth.lo-li.art:2083/oauth/authorize',
-    'oauth_token_url' => 'https://oauth.lo-li.art:2083/oauth/token',
-    'oauth_user_url' => 'https://oauth.lo-li.art:2083/api/user',
+    'callback_uri' => env('OAUTH_REDIRECT'),
+    'client_id' => env('OAUTH_CLIENT_ID'),
+    'client_secret' => env('OAUTH_CLIENT_SECRET'),
+    'oauth_auth_url' => env('OAUTH_DOMAIN') . '/oauth/authorize',
+    'oauth_token_url' => env('OAUTH_DOMAIN') . '/oauth/token',
+    'oauth_user_url' => env('OAUTH_DOMAIN') . '/api/user',
 ];
