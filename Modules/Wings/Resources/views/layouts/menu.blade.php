a99fb52bf06bfe78fba1a43f68d1e8ede44c34da
iVampireSP.com <im@ivampiresp.com>
2021-12-04 23:00:44 +0800

    预先准备菜单

--- Modules/Wings/Resources/views/layouts/menu.blade.php
+++ Modules/Wings/Resources/views/layouts/menu.blade.php
@@ -6,10 +6,52 @@
                 <span class="mdui-list-item-icon mdui-icon material-icons-outlined">dashboard</span>
                 <div class="mdui-list-item-content">Pterodactyl Wings</div>
             </a>
+
             <a class="mdui-list-item mdui-ripple" href="#">
                 <span class="mdui-list-item-icon mdui-icon material-icons-outlined">?</span>
-                <div class="mdui-list-item-content">Instances</div>
+                <div class="mdui-list-item-content">{{ tr('All Servers') }}</div>
             </a>
+
+            <a class="mdui-list-item mdui-ripple" href="#">
+                <span class="mdui-list-item-icon mdui-icon material-icons-outlined">?</span>
+                <div class="mdui-list-item-content">{{ tr('Create Server') }}</div>
+            </a>
+
+            <a class="mdui-list-item mdui-ripple" href="#">
+                <span class="mdui-list-item-icon mdui-icon material-icons-outlined">?</span>
+                <div class="mdui-list-item-content">{{ tr('Browse Servers') }}</div>
+            </a>
+
+
+            <div class="mdui-collapse-item">
+                <div class="mdui-collapse-item-header mdui-list-item mdui-ripple">
+                    <i class="mdui-list-item-icon mdui-icon material-icons">?</i>
+                    <div class="mdui-list-item-content">{{ tr('Panel Management') }}</div>
+                    <i class="mdui-collapse-item-arrow mdui-icon material-icons">keyboard_arrow_down</i>
+                </div>
+                <div class="mdui-collapse-item-body mdui-list">
+                    <a href="#" class="mdui-list-item mdui-ripple">{{ tr('Users') }}</a>
+                </div>
+
+                <div class="mdui-collapse-item-body mdui-list">
+                    <a href="#" class="mdui-list-item mdui-ripple">{{ tr('Servers') }}</a>
+                </div>
+            </div>
+
+            <div class="mdui-collapse-item">
+                <div class="mdui-collapse-item-header mdui-list-item mdui-ripple">
+                    <i class="mdui-list-item-icon mdui-icon material-icons">?</i>
+                    <div class="mdui-list-item-content">{{ tr('Server Management') }}</div>
+                    <i class="mdui-collapse-item-arrow mdui-icon material-icons">keyboard_arrow_down</i>
+                </div>
+                <div class="mdui-collapse-item-body mdui-list">
+                    <a href="#" class="mdui-list-item mdui-ripple">{{ tr('Locations') }}</a>
+                    <a href="#" class="mdui-list-item mdui-ripple">{{ tr('Nodes') }}</a>
+                    <a href="#" class="mdui-list-item mdui-ripple">{{ tr('Allocations') }}</a>
+                    <a href="#" class="mdui-list-item mdui-ripple">{{ tr('Nests') }}</a> {{-- Eggs in Nests --}}
+
+                </div>
+            </div>
         </div>
     </div>
 </div>
