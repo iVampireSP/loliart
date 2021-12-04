6593b55d91e9f6fe919289a7e22b231a6db8db0f
iVampireSP.com <im@ivampiresp.com>
2021-12-04 20:02:12 +0800

    独立 Home 链接菜单

---
+++ resources/views/components/home-menu-item.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <a class="mdui-list-item mdui-ripple" href="{{ route('main.index') }}">
+        <span class="mdui-list-item-icon mdui-icon material-icons-outlined">home</span>
+        <div class="mdui-list-item-content">{{ config('app.name') }}</div>
+    </a>
+</div>
