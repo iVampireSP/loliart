7190f8183e4fec5e17755d17964bf80988fc9831
iVampireSP.com <im@ivampiresp.com>
2021-12-04 18:44:23 +0800

    默认路由名称

--- Modules/Wings/Routes/web.php
+++ Modules/Wings/Routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::domain('wings.' . config('app.domain'))->prefix('/')->name('wings.')->group(function () {
-    Route::get('/', 'PlayListController@index');
+    Route::get('/', 'WingsController@index');
 });
