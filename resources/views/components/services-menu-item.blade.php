424ea6163c47d561f1b93232e163b57310490b2e
iVampireSP.com <im@ivampiresp.com>
2021-12-04 20:15:39 +0800

    增加 服务 菜单

---
+++ resources/views/components/services-menu-item.blade.php
@@ -0,0 +1,10 @@
+<div class="mdui-collapse-item">
+    <div class="mdui-collapse-item-header mdui-list-item mdui-ripple">
+        <i class="mdui-list-item-icon mdui-icon material-icons">apps</i>
+        <div class="mdui-list-item-content">服务</div>
+        <i class="mdui-collapse-item-arrow mdui-icon material-icons">keyboard_arrow_down</i>
+    </div>
+    <div class="mdui-collapse-item-body mdui-list">
+        {{-- <a href="{{ route('teams.index') }}" class="mdui-list-item mdui-ripple">{{ tr('Teams') }}</a> --}}
+    </div>
+</div>
