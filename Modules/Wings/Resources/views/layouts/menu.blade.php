c11b437af2c9a48bf1961234c5d8c050db5afd51
iVampireSP.com <im@ivampiresp.com>
2021-12-04 20:02:49 +0800

    修改 Home Menu Item

--- Modules/Wings/Resources/views/layouts/menu.blade.php
+++ Modules/Wings/Resources/views/layouts/menu.blade.php
@@ -1,10 +1,7 @@
 <div>
     <div class="mdui-list" mdui-collapse="{accordion: true}" style="margin-bottom: 76px;">
         <div class="mdui-list" id="main-list" mdui-collapse="{accordion: true}">
-            <a class="mdui-list-item mdui-ripple" href="{{ route('main.index') }}">
-                <span class="mdui-list-item-icon mdui-icon material-icons-outlined">home</span>
-                <div class="mdui-list-item-content">{{ config('app.name') }}</div>
-            </a>
+            <x-home-menu-item />
             @guest
                 <a class="mdui-list-item mdui-ripple" href="{{ route('login.redirect') }}">
                     <span class="mdui-list-item-icon mdui-icon material-icons-outlined">login</span>
