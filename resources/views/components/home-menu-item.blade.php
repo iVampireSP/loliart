bd9b2bfdb3ba7b8569452cdefe3e9b6471271c92
iVampireSP.com <im@ivampiresp.com>
2021-12-04 20:09:29 +0800

    将登录菜单项目移动至Home Menu Item

--- resources/views/components/home-menu-item.blade.php
+++ resources/views/components/home-menu-item.blade.php
@@ -3,4 +3,11 @@
         <span class="mdui-list-item-icon mdui-icon material-icons-outlined">home</span>
         <div class="mdui-list-item-content">{{ config('app.name') }}</div>
     </a>
+
+    @guest
+        <a class="mdui-list-item mdui-ripple" href="{{ route('login.redirect') }}">
+            <span class="mdui-list-item-icon mdui-icon material-icons-outlined">login</span>
+            <div class="mdui-list-item-content">{{ tr('Login or register') }}</div>
+        </a>
+    @endguest
 </div>
