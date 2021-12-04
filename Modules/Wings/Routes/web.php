2f2bb17494c8cdb6436dd910a46367fff2bfaabc
iVampireSP.com <im@ivampiresp.com>
2021-12-04 20:09:39 +0800

    设置路由名称

--- Modules/Wings/Routes/web.php
+++ Modules/Wings/Routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::domain('wings.' . config('app.domain'))->prefix('/')->name('wings.')->group(function () {
-    Route::get('/', 'WingsController@index');
+    Route::get('/', 'WingsController@index')->name('index');
 });
