9636de2500e6b79f89c64e200f78df840d68e4e6
iVampireSP.com <im@ivampiresp.com>
2021-12-04 18:43:51 +0800

    增加 Pterodactyl Wings 模块

---
+++ Modules/Wings/Routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::domain('wings.' . config('app.domain'))->prefix('/')->name('wings.')->group(function () {
+    Route::get('/', 'PlayListController@index');
+});
