fd301d6826a027b37b96ef82edebc125203e7c09
iVampireSP.com <im@ivampiresp.com>
2021-12-05 17:14:44 +0800

    移动配置文件位置

--- Modules/Wings/Config/config.php
+++ Modules/Wings/Config/config.php
@@ -1,5 +1,7 @@
 <?php
 
 return [
-    'name' => 'Wings'
+    'name' => 'Wings',
+    'url' => env('PANEL_URL'),
+    'key' => env('PANEL_API_KEY'),
 ];
--- Modules/Wings/Config/panel.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'url' => env('PANEL_URL'),
-    'key' => env('PANEL_API_KEY'),
-];
