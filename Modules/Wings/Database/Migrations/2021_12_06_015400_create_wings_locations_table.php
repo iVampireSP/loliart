bee47b3f96cda72754bacba70cb6bfac0e241a2e
iVampireSP.com <im@ivampiresp.com>
2021-12-06 16:22:05 +0800

    locations 迁移

---
+++ Modules/Wings/Database/Migrations/2021_12_06_015400_create_wings_locations_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWingsLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wings_locations', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name')->index();
+            $table->string('short')->index();
+
+            $table->unsignedBigInteger('location_id')->index()->nullable();
+
+            $table->unsignedBigInteger('team_id')->index();
+            $table->foreign('team_id')->references('id')->on('teams');
+
+            $table->string('status')->index()->default('pending');
+
+            $table->boolean('share')->index()->default(false);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wings_locations');
+    }
+}
