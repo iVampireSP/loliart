75a0d9d9008207bf92f7dc1576c4f61637e7af2b
iVampireSP.com <im@ivampiresp.com>
2021-12-06 14:02:38 +0800

    修改名称

--- Modules/Wings/Entities/WingsLocation.php
+++ Modules/Wings/Entities/WingsLocation.php
@@ -5,14 +5,9 @@ namespace Modules\Wings\Entities;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class WingsLocations extends Model
+class WingsLocation extends Model
 {
     use HasFactory;
 
     protected $fillable = [];
-    
-    protected static function newFactory()
-    {
-        return \Modules\Wings\Database\factories\WingsLocationsFactory::new();
-    }
 }
