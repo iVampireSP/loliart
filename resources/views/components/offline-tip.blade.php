c83e50d9cc3d7d4b27c5f4bea5666fdc78e3cb17
iVampireSP.com <im@ivampiresp.com>
2021-12-07 15:57:13 +0800

    网络失败视图

---
+++ resources/views/components/offline-tip.blade.php
@@ -0,0 +1,10 @@
+<div>
+    <div id="offline_tip">
+        <div class="window">
+            <h1 style="font-weight: 200;font-size: 3rem">{{ tr('Waiting for network') }}...</h1>
+            <p>{{ config('app.name', 'The application') . ' ' . tr('can not run if you are offline.') }}</p>
+        </div>
+
+    </div>
+
+</div>
