8a479b01911b2e9e3e760bd33b744b484e57adb0
iVampireSP.com <im@ivampiresp.com>
2021-12-07 12:32:53 +0800

    队列

---
+++ Modules/Wings/Jobs/RefreshWingNodeJob.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Modules\Wings\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Modules\Wings\Http\Controllers\PanelController;
+
+class RefreshWingNodeJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $id;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $panel = new PanelController();
+        $data = $panel->node($this->id, false);
+        Cache::put('wings_node_' . $this->id, $data, 600);
+    }
+}
