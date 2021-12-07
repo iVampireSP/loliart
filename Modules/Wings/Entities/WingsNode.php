0ab5e1eab1d0b7e2200dac144bbbd7e503cd6a6b
iVampireSP.com <im@ivampiresp.com>
2021-12-07 19:06:35 +0800

    快速填充字段

--- Modules/Wings/Entities/WingsNode.php
+++ Modules/Wings/Entities/WingsNode.php
@@ -9,7 +9,21 @@ class WingsNode extends Model
 {
     use HasFactory;
 
-    protected $fillable = [];
+    protected $fillable = [
+        'name',
+        'location_id',
+        'fqdn',
+        'memory',
+        'memory_overallocate',
+        'disk',
+        'disk_overallocate',
+        'upload_size',
+        'daemon_sftp',
+        'daemon_listen',
+        'behind_proxy',
+        'visibility',
+        'maintenance_mode',
+    ];
 
     public function location()
     {
