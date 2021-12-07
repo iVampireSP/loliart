0a78193a1565a1604704059f0ff045fdb2567027
iVampireSP.com <im@ivampiresp.com>
2021-12-07 12:33:16 +0800

    任务控制器

---
+++ Modules/Wings/Http/Controllers/JobController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Modules\Wings\Http\Controllers;
+
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Modules\Wings\Entities\WingsNode;
+use Modules\Wings\Jobs\RefreshWingNodeJob;
+
+class JobController extends Controller
+{
+    // Refresh wings data
+    public static function refresh_wings($id = null)
+    {
+        if (is_null($id)) {
+            // refresh all
+            $nodes = WingsNode::all();
+            WingsNode::chunk(100, function () use ($nodes) {
+                dispatch(new RefreshWingNodeJob($nodes->id));
+            });
+        } else {
+            if (WingsNode::where('id', $id)->exists()) {
+                dispatch(new RefreshWingNodeJob($id));
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+}
