0b4b2dd44be61ec89402a9514e86784b6f372311
iVampireSP.com <im@ivampiresp.com>
2021-12-07 21:42:32 +0800

    辅助函数 检测用户是否在团队中'

--- app/Helpers.php
+++ app/Helpers.php
@@ -4,6 +4,7 @@
  * Helpers
  */
 
+use App\Models\TeamUser;
 use App\Http\Controllers\AppController;
 use App\Http\Controllers\TranslateController;
 
@@ -27,5 +28,12 @@ if (!function_exists('avatar')) {
         $address = strtolower(trim($email));
         $hash = md5($address);
         return config('avatar.gravatar') . '/' . $hash;
+    }
+}
+
+if (!function_exists('userInTeam')) {
+    function userInTeam($team_id)
+    {
+        return TeamUser::where('team_id', $team_id)->where('user_id', auth()->id())->exists();
     }
 }
