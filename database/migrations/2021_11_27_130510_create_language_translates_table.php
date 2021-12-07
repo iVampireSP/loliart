602d1c4b6e72cb3d3da2aa61429109744e4280df
iVampireSP.com <im@ivampiresp.com>
2021-12-07 14:37:45 +0800

    增加 长度

--- database/migrations/2021_11_27_130510_create_language_translates_table.php
+++ database/migrations/2021_11_27_130510_create_language_translates_table.php
@@ -17,8 +17,8 @@ class CreateLanguageTranslatesTable extends Migration
             $table->id();
 
             $table->string('language')->index();
-            $table->string('string')->index();
-            $table->string('output')->index();
+            $table->string('string', 512)->index();
+            $table->string('output', 512)->index();
 
             $table->timestamps();
         });
