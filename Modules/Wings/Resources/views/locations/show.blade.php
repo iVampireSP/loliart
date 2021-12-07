23feded5b988716f23702503ef3a5c91ddeefd86
iVampireSP.com <im@ivampiresp.com>
2021-12-07 21:48:12 +0800

    显示优化

--- Modules/Wings/Resources/views/locations/show.blade.php
+++ Modules/Wings/Resources/views/locations/show.blade.php
@@ -30,6 +30,33 @@
             <div class="mdui-typo-display-1">{{ $location->name }}</div>
         @endcan
 
+        <div class="mdui-table-fluid">
+            <table class="mdui-table mdui-table-hoverable">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>{{ tr('Name') }}</th>
+                        <th>{{ tr('Servers') }}</th>
+                        <th>{{ tr('Disk space allocated') }}</th>
+                        <th>{{ tr('Memory allocated') }}</th>
+                    </tr>
+                </thead>
+                <tbody class="mdui-typo">
+                    @foreach ($nodes as $node)
+                        <tr id="node-{{ $node->id }}}">
+                            <td nowrap>{{ $location->id }}</td>
+                            <td nowrap><a
+                                    href="{{ route('wings.locations.nodes.show', [$node->location_id, $node->id]) }}">{{ $node->name }}</a>
+                            </td>
+                            <td nowrap>0</td>
+                            <td nowrap>0</td>
+                            <td nowrap>0</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+
 
     @elseif ($location->status == 'pending')
         <div class="mdui-typo-display-1">{{ tr('Please wait for process.') }}</div>
