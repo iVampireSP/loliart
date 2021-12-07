1cf48a7814d9e11f2b25882dc28bcb2032d05fa3
iVampireSP.com <im@ivampiresp.com>
2021-12-07 16:00:57 +0800

    引入缓存

--- Modules/Wings/Http/Controllers/PanelController.php
+++ Modules/Wings/Http/Controllers/PanelController.php
@@ -6,6 +6,7 @@ use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Client\RequestException;
 
@@ -45,6 +46,24 @@ class PanelController extends Controller
     public function deleteLocation($id)
     {
         return $this->delete('/locations/' . $id);
+    }
+
+    // Nodes
+    public function nodes($cache = true)
+    {
+        return $this->get('/nodes');
+    }
+
+    public function node($id, $cache = true)
+    {
+        $cache_key = 'wings_node_' . $id;
+        // return Cache::get($cache_key);
+        if ($cache) {
+            return Cache::get($cache_key);
+        } else {
+            Cache::put($cache_key, $this->get('/nodes/' . $id), 600);
+            return $this->get('/nodes/' . $id);
+        }
     }
 
     public function get($url, $data = null)
