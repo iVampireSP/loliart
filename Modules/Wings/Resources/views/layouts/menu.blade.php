2d62148a074a73ac3572f1a3fbec7a1a247f4eb3
iVampireSP.com <im@ivampiresp.com>
2021-12-08 19:30:58 +0800

    改进菜单项目

--- Modules/Wings/Resources/views/layouts/menu.blade.php
+++ Modules/Wings/Resources/views/layouts/menu.blade.php
@@ -35,9 +35,7 @@
                 </div>
                 <div class="mdui-collapse-item-body mdui-list">
                     <a href="{{ route('wings.locations.index') }}"
-                        class="mdui-list-item mdui-ripple">{{ tr('Locations') }}</a>
-                    <a href="#" class="mdui-list-item mdui-ripple">{{ tr('Nodes') }}</a>
-                    <a href="#" class="mdui-list-item mdui-ripple">{{ tr('Allocations') }}</a>
+                        class="mdui-list-item mdui-ripple">{{ tr('Nodes') }}</a>
                     <a href="#" class="mdui-list-item mdui-ripple">{{ tr('Nests') }}</a> {{-- Eggs in Nests --}}
 
                 </div>
