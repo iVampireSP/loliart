4863056339fdd1864eceae11e5f2625315a60997
iVampireSP.com <im@ivampiresp.com>
2021-12-08 12:53:45 +0800

    移除测试数据

--- Modules/Wings/Http/Controllers/WingsController.php
+++ Modules/Wings/Http/Controllers/WingsController.php
@@ -14,46 +14,6 @@ class WingsController extends Controller
      */
     public function index()
     {
-        $panel = new PanelController();
-        /**
-         * {
-  "name": "Test Renamed",
-  "description": "Test",
-  "location_id": 1,
-  "fqdn": "pterodactyl.file.properties",
-  "scheme": "https",
-  "behind_proxy": false,
-  "maintenance_mode": false,
-  "memory": 2048,
-  "memory_overallocate": 0,
-  "disk": 5000,
-  "disk_overallocate": 0,
-  "upload_size": 100,
-  "daemon_sftp": 2022,
-  "daemon_listen": 8080
-}
-         */
-        $data = json_decode('{
-"type": "update",
-"team_id": 56,
-"name": "test11",
-"description": "123",
-"display_name": "123",
-"location_id": 68,
-"fqdn": "nat.nwl.im",
-"scheme": "https",
-"memory": "1024",
-"memory_overallocate": "0",
-"disk": "10240",
-"disk_overallocate": "0",
-"upload_size": "100",
-"daemon_sftp": "2022",
-"daemon_listen": "8080",
-"visibility": 0,
-"behind_proxy": 0
-}', true);
-        // dd($data);
-        // $panel->updateNode(17, $data);
         return view('wings::index');
     }
 
