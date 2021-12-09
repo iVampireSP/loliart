96aac838ea3d54b0995bf61a8fc58a644fc97419
iVampireSP.com <im@ivampiresp.com>
2021-12-09 14:43:35 +0800

    双表迁移

---
+++ Modules/Wings/Database/Migrations/2021_12_09_055710_create_wings_nests_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWingsNestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wings_nests', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name')->index();
+            $table->string('description')->index()->nullable();
+            $table->string('author')->index();
+
+            $table->unsignedInteger('nest_id')->index();
+            $table->boolean('display')->default(true)->index();
+            $table->boolean('found')->default(true)->index();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wings_nests');
+    }
+}
---
+++ Modules/Wings/Database/Migrations/2021_12_09_055928_create_wings_nest_eggs_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWingsNestEggsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wings_nest_eggs', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name')->index();
+            $table->string('description', 512)->index();
+            $table->string('author')->index();
+            $table->string('docker_image')->index();
+            $table->string('startup', 512)->index();
+            $table->json('environment')->nullable();
+
+            $table->unsignedInteger('nest_id')->index()->nullable();
+
+            $table->unsignedInteger('egg_id')->index();
+            $table->boolean('can_use')->default(true)->index();
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wings_nest_eggs');
+    }
+}
