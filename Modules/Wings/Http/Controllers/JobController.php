68699e8613bf3361827ba05012d1de43378f8054
iVampireSP.com <im@ivampiresp.com>
2021-12-09 14:43:27 +0800

    筛选已删除的Nest

--- Modules/Wings/Http/Controllers/JobController.php
+++ Modules/Wings/Http/Controllers/JobController.php
@@ -2,11 +2,12 @@
 
 namespace Modules\Wings\Http\Controllers;
 
-use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Modules\Wings\Entities\WingsNest;
 use Modules\Wings\Entities\WingsNode;
 use Modules\Wings\Jobs\RefreshWingNodeJob;
+use Illuminate\Contracts\Support\Renderable;
 
 class JobController extends Controller
 {
@@ -29,5 +30,66 @@ class JobController extends Controller
                 return false;
             }
         }
+    }
+
+    // Refresh Nests and Eggs
+    public static function refresh_nests()
+    {
+        $panel = new PanelController();
+        $nests = (object)$panel->nests();
+        if (!$nests) {
+            return 0;
+        }
+        $arr = self::search($nests);
+
+        $ids = [];
+        foreach ($arr as $a) {
+            $wingsNest = new WingsNest();
+            $attr = $a['attributes'];
+            $create_data = [
+                'nest_id' => $attr['id'],
+                'author' => $attr['author'],
+                'name' => $attr['name'],
+                'description' => $attr['description'],
+            ];
+            $wingsNest_where = $wingsNest->where('nest_id', $attr['id']);
+            if ($wingsNest_where->exists()) {
+                $ids[] = $attr['id'];
+                $wingsNest_where->update($create_data);
+            } else {
+                $wingsNest->create($create_data);
+            }
+        }
+        $not_found = WingsNest::whereNotIn('nest_id', $ids)->update(['found' => 0]);
+
+        return $arr;
+    }
+
+    public static function refresh_eggs($nest_id)
+    {
+        $panel = new PanelController();
+        $eggs = (object)$panel->eggs($nest_id);
+        return $eggs;
+    }
+
+    protected static function search(object $data)
+    {
+        $next_page = 1;
+        $is_continue = true;
+        $arr = [];
+        do {
+            $total_page = $data->meta['pagination']['total_pages'];
+            if ($next_page == $total_page) {
+                $is_continue = false;
+            } else {
+                $next_page = $data->meta['pagination']['current_page'] + 1;
+            }
+
+            foreach ($data->data as $d) {
+                array_push($arr, $d);
+            }
+        } while ($is_continue);
+
+        return $arr;
     }
 }
