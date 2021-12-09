112f6316750b533bcce15a9fe8b1e1e3704c8b31
iVampireSP.com <im@ivampiresp.com>
2021-12-09 22:06:16 +0800

    显示 节点资源信息

--- Modules/Wings/Resources/views/nodes/show.blade.php
+++ Modules/Wings/Resources/views/nodes/show.blade.php
@@ -10,12 +10,77 @@
     <div class="mdui-typo-display-1">{{ $node->display_name }}</div>
 
     <div class="mdui-tab" mdui-tab>
-        <a href="#node-info" class="mdui-ripple">{{ tr('Node Info') }}</a>
-        <a href="#node-config" class="mdui-ripple">{{ tr('Node Configuration') }}</a>
+        <a href="#node-about" class="mdui-ripple">{{ tr('About') }}</a>
+        <a href="#node-settings" class="mdui-ripple">{{ tr('Settings') }}</a>
+        <a href="#node-config" class="mdui-ripple">{{ tr('Configuration') }}</a>
+        <a href="#node-allocation" class="mdui-ripple">{{ tr('Allocations') }}</a>
         <a href="#delete-node" class="mdui-ripple">{{ tr('Delete Node') }}</a>
     </div>
 
-    <div class="mdui-row mdui-m-t-5" id="node-info">
+    <div id="node-about">
+        <div class="mdui-col-md-6 mdui-col-sm-12">
+            <div class="mdui-typo-headline mdui-m-t-3">{{ tr('Information') }}</div>
+
+            <div class="mdui-table-fluid mdui-m-t-2">
+                <table class="mdui-table mdui-table-hoverable">
+                    <tbody class="mdui-typo">
+                        <tr>
+                            <td>{{ tr('Daemon Version') }}</td>
+                            <td>{{ $node->version }}</td>
+                        </tr>
+                        <tr>
+                            <td>{{ tr('System Information') }}</td>
+                            <td>{{ $node->os }} ({{ $node->architecture }})
+                                <code>{{ $node->kernel_version }}</code>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>{{ tr('Total CPU Threads') }}</td>
+                            <td>{{ $node->cpu_count }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <div class="mdui-col-md-6 mdui-col-sm-12">
+            <div class="mdui-typo-headline mdui-m-t-3">At-a-Glance</div>
+            @php($allocated = Cache::get('wings_node_' . $node->node_id)['attributes']['allocated_resources'] ?? ['memory' => 0, 'disk' => 0])
+            <div class="mdui-table-fluid mdui-m-t-2">
+                <table class="mdui-table mdui-table-hoverable">
+                    <tbody class="mdui-typo">
+                        <tr>
+                            <td>{{ tr('DISK SPACE ALLOCATED') }}</td>
+                            <td>
+                                {{ $allocated['disk'] }} / {{ $node->disk }} Mb
+                                <div class="mdui-progress">
+                                    <div class="mdui-progress-determinate"
+                                        style="width: {{ ($allocated['disk'] / $node->disk) * 100 }}%;"></div>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>{{ tr('MEMORY ALLOCATED') }}</td>
+                            <td>
+                                {{ $allocated['memory'] }} / {{ $node->memory }} Mb
+                                <div class="mdui-progress">
+                                    <div class="mdui-progress-determinate"
+                                        style="width: {{ ($allocated['memory'] / $node->memory) * 100 }}%;"></div>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>{{ tr('TOTAL SERVERS') }}</td>
+                            <td>0</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="mdui-row mdui-m-t-5" id="node-settings">
         <x-lock for="node-edit" lock="1" />
 
         <form action="#" data-lock-form="node-edit"
@@ -166,6 +231,8 @@
             <pre>{!! $node_configuration !!}</pre>
         </div>
     </div>
+
+    <div id="node-allocation"></div>
 
     <div id="delete-node" class="mdui-m-t-5">
         <button class="mdui-btn btn-ripple mdui-btn-outlined"
