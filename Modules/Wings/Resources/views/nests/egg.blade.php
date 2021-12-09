bb17482c287e402c6a4cdab7f794098a7f28efb5
iVampireSP.com <im@ivampiresp.com>
2021-12-09 18:54:03 +0800

    判断 egg 是否能使用

--- Modules/Wings/Resources/views/nests/egg.blade.php
+++ Modules/Wings/Resources/views/nests/egg.blade.php
@@ -1,15 +1,21 @@
 @extends('layouts.app')
 
-@section('title', $egg->name)
+@section('title', 'Egg: ' . $egg->name)
 
 @section('app-menu')
     @include('wings::layouts.menu')
 @endsection
 
 @section('content')
+    @if (!$egg->can_use)
+        <div class="mdui-chip">
+            <span class="mdui-chip-icon mdui-color-blue">
+                <i class="mdui-icon material-icons">error</i>
+            </span>
+            <span class="mdui-chip-title">{{ tr('This egg is temporarily unavailable.') }}</span>
+        </div>
+    @endif
     <div class="mdui-typo-headline">{{ $egg->name }}</div>
-    <div class="mdui-typo-body-1">{{ $egg->description }}</div>
-
-
+    <div class="mdui-typo-body-1">{{ tr($egg->description) }}</div>
 
 @endsection
