5c9f7d4cd4eaece59b6f5cc0b5a9c91fbe237116
iVampireSP.com <im@ivampiresp.com>
2021-12-09 15:38:43 +0800

    计划任务

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,11 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+
+        $schedule->call(function () {
+            \Modules\Wings\Http\Controllers\JobController::refresh_wings();
+            dispatch(new \Modules\Wings\Jobs\RefreshNestJob());
+        })->hourly();
     }
 
     /**
@@ -25,7 +30,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
