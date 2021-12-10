4f77ae560c1b399d191f94fefdd0a28c48f00759
iVampireSP.com <im@ivampiresp.com>
2021-12-10 13:13:30 +0800

    增加 更新 Allocation

--- Modules/Wings/Jobs/UpdateNodeInfoJob.php
+++ Modules/Wings/Jobs/UpdateNodeInfoJob.php
@@ -7,7 +7,9 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Modules\Wings\Entities\WingsAllocation;
 use Modules\Wings\Entities\WingsNode;
+use Modules\Wings\Http\Controllers\JobController;
 use Modules\Wings\Http\Controllers\PanelController;
 
 class UpdateNodeInfoJob implements ShouldQueue
@@ -47,6 +49,49 @@ class UpdateNodeInfoJob implements ShouldQueue
                     $node->active = 1;
                     $node->save();
                 }
+
+                // 更新 Allocation
+                $next_page = 1;
+                $is_continue = true;
+                $allocation_ids = [];
+                do {
+                    $allocations = (object)$panel->allocations($node->node_id, $next_page);
+                    $total_page = $allocations->meta['pagination']['total_pages'];
+                    if ($next_page == $total_page) {
+                        $is_continue = false;
+                    } else {
+                        $next_page = $allocations->meta['pagination']['current_page'] + 1;
+                    }
+                    foreach ($allocations->data as $allocation) {
+                        $allocation = (object)$allocation['attributes'];
+
+                        $resp = [
+                            'ip' => $allocation->ip,
+                            'alias' => $allocation->alias,
+                            'port' => $allocation->port,
+                            'node_id' => $node->id,
+                            'allocation_id' => $allocation->id,
+                        ];
+
+                        $orm = WingsAllocation::where('allocation_id', $allocation->id);
+
+                        if ($orm->exists()) {
+                            $allocation_ids[] = $allocation->id;
+                            $resp['found'] = 1;
+                            $orm->update($resp);
+                        } else {
+                            $allocation_ids[] = $allocation->id;
+                            WingsAllocation::create($resp);
+                        }
+
+
+                        // if (!$allocation['attributes']['assigned']) {
+                        //     $selected_id = $allocation['attributes']['id'];
+                        //     return $selected_id;
+                        // }
+                    }
+                } while ($is_continue);
+                WingsAllocation::whereNotIn('allocation_id', $allocation_ids)->update(['found' => 0]);
             }
         });
     }
