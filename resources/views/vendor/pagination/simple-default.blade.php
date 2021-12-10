93e7b82da786d049305cfaf623f8ea43da7a1a0a
iVampireSP.com <im@ivampiresp.com>
2021-12-10 20:18:43 +0800

    应用翻页

--- resources/views/vendor/pagination/simple-default.blade.php
+++ resources/views/vendor/pagination/simple-default.blade.php
@@ -1,17 +1,19 @@
 <div class="mdui-m-t-2">
     @if ($paginator->hasPages())
         @if ($paginator->onFirstPage())
-            <button disabled class="mdui-float-left mdui-btn mdui-color-theme-accent mdui-ripple">上一页</button>
+            <button disabled
+                class="mdui-float-left mdui-btn mdui-color-theme-accent mdui-ripple">{{ tr('Previous page') }}</button>
         @else
             <a href="{{ $paginator->previousPageUrl() }}"
-                class="mdui-float-left mdui-btn mdui-color-theme-accent mdui-ripple">上一页</a>
+                class="mdui-float-left mdui-btn mdui-color-theme-accent mdui-ripple">{{ tr('Previous page') }}</a>
         @endif
 
         @if ($paginator->hasMorePages())
             <a href="{{ $paginator->nextPageUrl() }}"
-                class="mdui-float-right mdui-btn mdui-color-theme-accent mdui-ripple">下一页</a>
+                class="mdui-float-right mdui-btn mdui-color-theme-accent mdui-ripple">{{ tr('Next page') }}</a>
         @else
-            <button disabled class="mdui-float-right mdui-btn mdui-color-theme-accent mdui-ripple">下一页</button>
+            <button disabled
+                class="mdui-float-right mdui-btn mdui-color-theme-accent mdui-ripple">{{ tr('Next page') }}</button>
         @endif
 
     @endif
