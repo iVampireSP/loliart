f90947b1025e27dd1bf4ee7ad57a56961221dc24
iVampireSP.com <im@ivampiresp.com>
2021-12-10 20:44:01 +0800

    改进 锁逻辑

--- resources/views/components/lock.blade.php
+++ resources/views/components/lock.blade.php
@@ -1,6 +1,7 @@
 <div>
-    <button class="mdui-btn mdui-btn-icon mdui-float-right" style="z-index: 1;"
-        onclick="util.toggleLock('{{ $for }}', true)" data-lock-btn="{{ $for }}"
+    <button data-lock-pass class="mdui-btn mdui-btn-icon mdui-float-right" style="z-index: 1;"
+        onclick="event.preventDefault();util.toggleLock('{{ $for }}', true)"
+        data-lock-btn="{{ $for }}"
         mdui-tooltip="{content: '{{ tr('Lock/Unlock') }}', position: 'left', delay: 1000}">
         <i class="mdui-icon material-icons">lock_open</i>
     </button>
