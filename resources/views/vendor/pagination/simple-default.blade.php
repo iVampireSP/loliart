8f2264e0247b7d9d77558558da26d16089cc3d39
iVampireSP.com <im@ivampiresp.com>
2021-12-10 20:40:01 +0800

    改进 按钮样式

--- resources/views/vendor/pagination/simple-default.blade.php
+++ resources/views/vendor/pagination/simple-default.blade.php
@@ -1,19 +1,17 @@
 <div class="mdui-m-t-2">
     @if ($paginator->hasPages())
         @if ($paginator->onFirstPage())
-            <button disabled
-                class="mdui-float-left mdui-btn mdui-color-theme-accent mdui-ripple">{{ tr('Previous page') }}</button>
+            <button disabled class="mdui-float-left mdui-btn mdui-ripple">{{ tr('Previous page') }}</button>
         @else
             <a onclick="util.url.to('{{ $paginator->previousPageUrl() }}', '.pages', '.pages-link')"
-                class="mdui-float-left mdui-btn mdui-color-theme-accent mdui-ripple">{{ tr('Previous page') }}</a>
+                class="mdui-float-left mdui-btn mdui-btn-outlined  mdui-ripple">{{ tr('Previous page') }}</a>
         @endif
 
         @if ($paginator->hasMorePages())
             <a onclick="util.url.to('{{ $paginator->nextPageUrl() }}', '.pages', '.pages-link')"
-                class="mdui-float-right mdui-btn mdui-color-theme-accent mdui-ripple">{{ tr('Next page') }}</a>
+                class="mdui-float-right mdui-btn mdui-btn-outlined mdui-ripple">{{ tr('Next page') }}</a>
         @else
-            <button disabled
-                class="mdui-float-right mdui-btn mdui-color-theme-accent mdui-ripple">{{ tr('Next page') }}</button>
+            <button disabled class="mdui-float-right mdui-btn mdui-ripple">{{ tr('Next page') }}</button>
         @endif
 
     @endif
