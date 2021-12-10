b5bf8844cc2dfc9af94274814a37e9868eae0f24
iVampireSP.com <im@ivampiresp.com>
2021-12-10 20:43:25 +0800

    改进 命名

--- resources/js/util.js
+++ resources/js/util.js
@@ -18,9 +18,9 @@ window.util = {
         let j = $(`${btn} i`)
         if (j.hasClass('locked')) {
             j.html('lock_open')
-            $(`${form} input:not([data-pass-lock])`).attr('readonly', false)
-            $(`${form} input:not([data-pass-lock]`).attr('disabled', false)
-            $(`${form} button:not([data-pass-lock]`).attr('disabled', false)
+            $(`${form} input:not([data-lock-pass])`).attr('readonly', false)
+            $(`${form} input:not([data-lock-pass]`).attr('disabled', false)
+            $(`${form} button:not([data-lock-pass]`).attr('disabled', false)
             if (play) {
                 util.play('unlock.mp3')
             }
@@ -28,9 +28,9 @@ window.util = {
         } else {
             j.html('lock')
             j.addClass('locked')
-            $(`${form} input:not([data-pass-lock]`).attr('disabled', true)
-            $(`${form} input:not([data-pass-lock]`).attr('readonly', true)
-            $(`${form} button:not([data-pass-lock]`).attr('disabled', true)
+            $(`${form} input:not([data-lock-pass]`).attr('disabled', true)
+            $(`${form} input:not([data-lock-pass]`).attr('readonly', true)
+            $(`${form} button:not([data-lock-pass]`).attr('disabled', true)
             if (play) {
                 util.play('lock.mp3')
             }
