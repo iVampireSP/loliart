97386896f0ef462350cca2027f3da9ab8d2341bc
iVampireSP.com <im@ivampiresp.com>
2021-12-11 19:44:13 +0800

    锁与优化

--- resources/views/permissions/edit.blade.php
+++ resources/views/permissions/edit.blade.php
@@ -3,33 +3,37 @@
 @section('title', tr('Role permissions'))
 
 @section('content')
-    <div class="mdui-typo-display-1">{{ tr('Role permissions') }}</div>
+    <div class="mdui-typo-display-1 mdui-m-b-2">{{ tr('Role permissions') }}</div>
 
-    @can('role.edit')
-        <button onclick="util.team.permission.role.givePermission('{{ $role->name }}')"
-            class="mdui-btn mdui-btn-outlined mdui-ripple ">{{ tr('Add Permission') }}</button>
+    <x-lock for="role-permissions" />
 
-        <button onclick="util.team.permission.role.delete({{ $role->id }})"
-            class="mdui-btn mdui-btn-outlined mdui-ripple ">{{ tr('Remove this role') }}</button>
-    @endcan
+    <div data-lock-form="role-permissions">
+        @can('role.edit')
+            <button onclick="util.team.permission.role.givePermission('{{ $role->name }}')"
+                class="mdui-btn mdui-btn-outlined mdui-ripple ">{{ tr('Add Permission') }}</button>
 
-    <ul class="mdui-list">
-        @foreach ($role->permissions as $permission)
-            <li class="mdui-list-item mdui-ripple">
-                <div class="mdui-list-item-content">
-                    <div class="mdui-list-item-title mdui-list-item-one-line">
-                        {{ tr('Permission name: ') }}{{ $permission->name }}</div>
-                    <div class="mdui-list-item-text mdui-list-item-two-line">{{ tr($permission->display_name) }}</div>
-                </div>
-                <a href="javascript: util.team.permission.role.removePermission({{ $role->id }}, '{{ $permission->name }}')"
-                    mdui-tooltip="{content: '{{ tr('Delete') }}', position: 'left'}"
-                    class="mdui-text-color-blue mdui-btn mdui-btn-icon mdui-ripple mdui-m-r-1"
-                    style="border-radius: 50px !important">
-                    <i class="mdui-list-item-icon mdui-icon material-icons">delete</i>
-                </a>
-            </li>
-        @endforeach
-    </ul>
+            <button onclick="util.team.permission.role.delete({{ $role->id }})"
+                class="mdui-btn mdui-btn-outlined mdui-ripple ">{{ tr('Remove this role') }}</button>
+        @endcan
 
+        <ul class="mdui-list">
+            @foreach ($role->permissions as $permission)
+                <li class="mdui-list-item mdui-ripple">
+                    <div class="mdui-list-item-content">
+                        <div class="mdui-list-item-title mdui-list-item-one-line">
+                            {{ tr('Permission name: ') }}{{ $permission->name }}</div>
+                        <div class="mdui-list-item-text mdui-list-item-two-line">{{ tr($permission->display_name) }}</div>
+                    </div>
+                    <a href="javascript: util.team.permission.role.removePermission({{ $role->id }}, '{{ $permission->name }}')"
+                        mdui-tooltip="{content: '{{ tr('Delete') }}', position: 'left'}"
+                        class="mdui-text-color-blue mdui-btn mdui-btn-icon mdui-ripple mdui-m-r-1"
+                        style="border-radius: 50px !important">
+                        <i class="mdui-list-item-icon mdui-icon material-icons">delete</i>
+                    </a>
+                </li>
+            @endforeach
+        </ul>
+
+    </div>
 
 @endsection
