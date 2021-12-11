ceb797e09101b8ec87056232244599d1130c400e
iVampireSP.com <im@ivampiresp.com>
2021-12-11 20:08:51 +0800

    调整命名

--- app/View/Components/ToolMenu.php
+++ app/View/Components/ToolMenu.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class tool-menu extends Component
+class ToolMenu extends Component
 {
     /**
      * Create a new component instance.
