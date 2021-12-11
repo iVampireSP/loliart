ed13594df5219fa057818b20dc8c8ff4ee0f3f8b
iVampireSP.com <im@ivampiresp.com>
2021-12-11 18:56:39 +0800

    修改名称

--- resources/views/components/services-menu-item.blade.php
+++ resources/views/components/services-menu-item.blade.php
@@ -1,7 +1,7 @@
 <div class="mdui-collapse-item">
     <div class="mdui-collapse-item-header mdui-list-item mdui-ripple">
         <i class="mdui-list-item-icon mdui-icon material-icons">apps</i>
-        <div class="mdui-list-item-content">服务</div>
+        <div class="mdui-list-item-content">{{ tr('Services') }}</div>
         <i class="mdui-collapse-item-arrow mdui-icon material-icons">keyboard_arrow_down</i>
     </div>
     <div class="mdui-collapse-item-body mdui-list">
