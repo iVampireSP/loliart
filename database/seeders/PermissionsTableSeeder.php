c770222963d18354a94f5474ad3bb7d5381842f8
iVampireSP.com <im@ivampiresp.com>
2021-12-12 14:46:07 +0800

    导出更新

--- database/seeders/PermissionsTableSeeder.php
+++ database/seeders/PermissionsTableSeeder.php
@@ -21,75 +21,102 @@ class PermissionsTableSeeder extends Seeder
         \DB::table('permissions')->insert(array (
             0 => 
             array (
-                'created_at' => '2021-12-01 03:03:38',
-                'display_name' => 'User from invitation',
-                'guard_name' => 'web',
                 'id' => 1,
                 'name' => 'team.invitations.access',
+                'display_name' => 'User from invitation',
+                'guard_name' => 'web',
+                'created_at' => '2021-12-01 03:03:38',
                 'updated_at' => '2021-12-01 03:03:38',
             ),
             1 => 
             array (
-                'created_at' => '2021-12-01 23:50:22',
-                'display_name' => 'Edit team settings',
-                'guard_name' => 'web',
                 'id' => 3,
                 'name' => 'team.edit',
+                'display_name' => 'Edit team settings',
+                'guard_name' => 'web',
+                'created_at' => '2021-12-01 23:50:22',
                 'updated_at' => '2021-12-01 23:50:22',
             ),
             2 => 
             array (
-                'created_at' => '2021-12-02 02:46:45',
-                'display_name' => 'Show role permissions',
-                'guard_name' => 'web',
                 'id' => 4,
                 'name' => 'role.show',
+                'display_name' => 'Show role permissions',
+                'guard_name' => 'web',
+                'created_at' => '2021-12-02 02:46:45',
                 'updated_at' => '2021-12-02 02:46:45',
             ),
             3 => 
             array (
-                'created_at' => '2021-12-02 02:47:27',
-                'display_name' => 'Edit role',
-                'guard_name' => 'web',
                 'id' => 5,
                 'name' => 'role.edit',
+                'display_name' => 'Edit role',
+                'guard_name' => 'web',
+                'created_at' => '2021-12-02 02:47:27',
                 'updated_at' => '2021-12-02 02:47:27',
             ),
             4 => 
             array (
-                'created_at' => '2021-12-07 14:37:13',
-                'display_name' => 'Access team',
-                'guard_name' => 'web',
                 'id' => 6,
                 'name' => 'team.access',
+                'display_name' => 'Access team',
+                'guard_name' => 'web',
+                'created_at' => '2021-12-07 14:37:13',
                 'updated_at' => '2021-12-07 14:37:13',
             ),
             5 => 
             array (
-                'created_at' => '2021-12-07 14:38:29',
-                'display_name' => 'Edit node data',
+                'id' => 8,
+                'name' => 'wings.accounts.edit',
+                'display_name' => 'Wings Accounts edit',
                 'guard_name' => 'web',
-                'id' => 7,
-                'name' => 'node.edit',
-                'updated_at' => '2021-12-07 14:38:29',
+                'created_at' => '2021-12-09 02:39:29',
+                'updated_at' => '2021-12-09 02:39:29',
             ),
             6 => 
             array (
-                'created_at' => '2021-12-09 02:39:29',
-                'display_name' => NULL,
+                'id' => 11,
+                'name' => 'wings.locations.edit',
+                'display_name' => 'Wings Locations edit',
                 'guard_name' => 'web',
-                'id' => 8,
-                'name' => 'wings.accounts.edit',
-                'updated_at' => '2021-12-09 02:39:29',
+                'created_at' => '2021-12-11 19:09:19',
+                'updated_at' => '2021-12-11 19:09:19',
             ),
             7 => 
             array (
-                'created_at' => '2021-12-09 02:39:34',
+                'id' => 12,
+                'name' => 'wings.nodes.edit',
+                'display_name' => 'Wings Nodes edit',
+                'guard_name' => 'web',
+                'created_at' => '2021-12-11 19:09:24',
+                'updated_at' => '2021-12-11 19:09:24',
+            ),
+            8 => 
+            array (
+                'id' => 13,
+                'name' => 'wings.servers.edit',
+                'display_name' => 'Wings Servers Edit',
+                'guard_name' => 'web',
+                'created_at' => '2021-12-11 19:09:29',
+                'updated_at' => '2021-12-11 19:09:29',
+            ),
+            9 => 
+            array (
+                'id' => 14,
+                'name' => 'wings.nodes.show',
+                'display_name' => 'Wings Nodes show config',
+                'guard_name' => 'web',
+                'created_at' => '2021-12-11 19:11:20',
+                'updated_at' => '2021-12-11 19:11:20',
+            ),
+            10 => 
+            array (
+                'id' => 15,
+                'name' => 'admin',
                 'display_name' => NULL,
                 'guard_name' => 'web',
-                'id' => 9,
-                'name' => 'wings.node.edit',
-                'updated_at' => '2021-12-09 02:39:34',
+                'created_at' => '2021-12-11 19:31:30',
+                'updated_at' => '2021-12-11 19:31:30',
             ),
         ));
         
