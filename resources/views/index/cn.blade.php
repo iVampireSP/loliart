fe2815416802c0a671ca932f57362b0f08bdfb6b
iVampireSP.com <im@ivampiresp.com>
2021-12-13 09:56:05 +0800

    CN 页面测试

---
+++ resources/views/index/cn.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', 'Edge Staging')
+
+@section('content')
+
+    <div class="mdui-typo-display-1">Edge Staging 版本</div>
+    <script>
+        util.m()
+    </script>
+@endsection
