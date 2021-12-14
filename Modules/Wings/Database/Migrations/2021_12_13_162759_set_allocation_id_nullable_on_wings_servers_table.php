4e99df01ec95701c0081236e09effef55bc29def
iVampireSP.com <im@ivampiresp.com>
2021-12-14 08:21:57 +0800

    设置 server_id 空

--- Modules/Wings/Database/Migrations/2021_12_13_162759_set_allocation_id_nullable_on_wings_servers_table.php
+++ Modules/Wings/Database/Migrations/2021_12_13_162759_set_allocation_id_nullable_on_wings_servers_table.php
@@ -15,6 +15,7 @@ class SetAllocationIdNullableOnWingsServersTable extends Migration
     {
         Schema::table('wings_servers', function (Blueprint $table) {
             $table->unsignedBigInteger('allocation_id')->nullable()->change();
+            $table->unsignedBigInteger('server_id')->nullable()->change();
         });
     }
 
