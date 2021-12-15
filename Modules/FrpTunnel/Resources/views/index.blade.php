4be3b541e289b4b236a603dc8875c9d75ccaa446
iVampireSP.com <im@ivampiresp.com>
2021-12-15 19:24:35 +0800

    添加 Frp Tunnel

---
+++ Modules/FrpTunnel/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('frptunnel::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('frptunnel.name') !!}
+    </p>
+@endsection
