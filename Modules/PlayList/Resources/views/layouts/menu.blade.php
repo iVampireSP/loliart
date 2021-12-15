f034a734ddcf372d5ca32b3ecc878468d34b31e0
iVampireSP.com <im@ivampiresp.com>
2021-12-15 19:24:11 +0800

    移除一个饼

--- Modules/PlayList/Resources/views/layouts/menu.blade.php
+++
@@ -1,7 +0,0 @@
-<div>
-    <div class="mdui-list" mdui-collapse="{accordion: true}" style="margin-bottom: 76px;">
-        <div class="mdui-list" id="main-list" mdui-collapse="{accordion: true}">
-            <x-home-menu-item />
-        </div>
-    </div>
-</div>
