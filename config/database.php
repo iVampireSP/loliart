cb2f972f9848ad13f8b9dc20f3f5b07c22a3530e
iVampireSP.com <im@ivampiresp.com>
2021-12-17 14:10:01 +0800

    修改数据库前缀

--- config/database.php
+++ config/database.php
@@ -126,7 +126,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', 'e2af_database'),
+            'prefix' => env('REDIS_PREFIX', 'e2af_database_'),
         ],
 
         'default' => [
