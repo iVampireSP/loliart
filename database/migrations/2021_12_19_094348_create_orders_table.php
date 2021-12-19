486f2ef8b7f5c2d51056dc60c250c4edaa52865d
iVampireSP.com <im@ivampiresp.com>
2021-12-19 09:56:34 +0800

    订单迁移

---
+++ database/migrations/2021_12_19_094348_create_orders_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name')->index();
+            $table->string('order_id')->index();
+
+            $table->string('comment')->index()->nullable();
+            $table->string('status')->index()->default('pending');
+
+            $table->string('product')->index()->nullable();
+            $table->unsignedBigInteger('product_id')->index()->nullable();
+
+            $table->unsignedBigInteger('user_id')->index()->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->unsignedBigInteger('team_id')->index()->nullable();
+            $table->foreign('team_id')->references('id')->on('teams');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
