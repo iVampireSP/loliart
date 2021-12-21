f2819fe548d47eedc534c3e401006e2d583aea87
iVampireSP.com <im@ivampiresp.com>
2021-12-21 10:20:40 +0800

    增加用户余额

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Cache\LockTimeoutException;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Cache;
 use Laravel\Cashier\Billable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
@@ -44,7 +45,30 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function orders() {
+    public function orders()
+    {
         return $this->hasMany(Order::class, 'user_id', 'id');
+    }
+
+    public function addBalance($amount, $user_id = false)
+    {
+        if (!$user_id) {
+            $user = auth()->user();
+        } else {
+            $user = self::find($user_id);
+        }
+        $lock = Cache::lock("user_balance_" . $user->id, $user->balance);
+        $lock->block(5);
+        try {
+            $user->balance += $amount;
+            $user->save();
+        } catch (LockTimeoutException $e) {
+            unset($e);
+            write('Unable to update user balance');
+            return false;
+        } finally {
+            optional($lock)->release();
+        }
+        return true;
     }
 }
