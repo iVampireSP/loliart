d952f04046c36ca31efa022d2dc5eff79b36b612
iVampireSP.com <im@ivampiresp.com>
2021-12-21 10:24:50 +0800

    注册 Stripe WebHook 监听

---
+++ app/Listeners/StripeEventListener.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Listeners;
+
+use Laravel\Cashier\Events\WebhookReceived;
+
+class StripeEventListener
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle(WebhookReceived $event)
+    {
+        if ($event->payload['type'] === 'invoice.payment_succeeded') {
+            // Handle the incoming event...
+        }
+    }
+}
