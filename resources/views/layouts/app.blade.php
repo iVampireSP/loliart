8f7e7e35f4cd484a355f20593289699a08594475
iVampireSP.com <im@ivampiresp.com>
2021-12-21 08:07:43 +0800

    避免浏览器解析字段

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1.0, user-scalable=no" />
     <meta name="format-detection" content="telephone=no" />
+    <meta name="format-detection" content="date=no" />
+    <meta name="format-detection" content="address=no" />
 
     <meta name="csrf-token" content="{!! csrf_token() !!}" />
 
