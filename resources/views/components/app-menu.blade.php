d8a4b1dff0da9647c0732d477b82477d67f482a7
iVampireSP.com <im@ivampiresp.com>
2021-12-22 11:39:44 +0800

    增加 订阅与订单

--- resources/views/components/app-menu.blade.php
+++ resources/views/components/app-menu.blade.php
@@ -23,17 +23,27 @@
             <div class="mdui-collapse-item">
                 <div class="mdui-collapse-item-header mdui-list-item mdui-ripple">
                     <i class="mdui-list-item-icon mdui-icon material-icons">account_circle</i>
-                    <div class="mdui-list-item-content">{{ auth()->user()->name ?? tr('Guest')}}</div>
+                    <div class="mdui-list-item-content">{{ auth()->user()->name ?? tr('Guest') }}</div>
                     <i class="mdui-collapse-item-arrow mdui-icon material-icons">keyboard_arrow_down</i>
                 </div>
                 <div class="mdui-collapse-item-body mdui-list">
                     <a href="{{ route('user.balance.manage') }}"
                         class="mdui-list-item mdui-ripple">{{ tr('Payment') }}</a>
                 </div>
+
+                <div class="mdui-collapse-item-body mdui-list">
+                    <a href="{{ route('user.subscriptions') }}"
+                        class="mdui-list-item mdui-ripple">{{ tr('Orders') }}</a>
+                </div>
+
+                <div class="mdui-collapse-item-body mdui-list">
+                    <a href="{{ route('user.subscriptions') }}"
+                        class="mdui-list-item mdui-ripple">{{ tr('Subscriptions') }}</a>
+                </div>
             </div>
 
-
         </div>
+
         <div class="edge-bottom">
             <div class="mdui-typo-caption-opacity mdui-text-center">&copy; 2021 Edge Stading. All rights reserved.
             </div>
