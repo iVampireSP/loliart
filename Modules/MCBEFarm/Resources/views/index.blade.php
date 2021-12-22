5086bb237a728aa221c2fed74f2156110c9f12f8
iVampireSP.com <im@ivampiresp.com>
2021-12-22 14:32:51 +0800

    MCBE Server Farm

---
+++ Modules/MCBEFarm/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('mcbefarm::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('mcbefarm.name') !!}
+    </p>
+@endsection
