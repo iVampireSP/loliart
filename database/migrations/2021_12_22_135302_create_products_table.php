8019e53d91b5b2a3b10226b68441115eeee39107
iVampireSP.com <im@ivampiresp.com>
2021-12-22 14:32:10 +0800

    产品模型和迁移

---
+++ database/migrations/2021_12_22_135302_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name')->index();
+            $table->string('price_id')->index();
+            $table->double('amount')->index()->nullable();
+            $table->string('type')->index();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
