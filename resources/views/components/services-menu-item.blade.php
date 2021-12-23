06940e9fb1a90c51ce3061f5234a833982172a8b
iVampireSP.com <im@ivampiresp.com>
2021-12-23 16:07:04 +0800

    Bedrock Server Flow(Farm)

--- resources/views/components/services-menu-item.blade.php
+++ resources/views/components/services-menu-item.blade.php
@@ -7,6 +7,23 @@
     <div class="mdui-collapse-item-body mdui-list">
         <a href="{{ route('wings.index') }}" class="mdui-list-item mdui-ripple">{{ tr('Game Containers') }}</a>
         <a href="{{ route('frpTunnel.index') }}" class="mdui-list-item mdui-ripple">{{ tr('Frp Tunnel') }}</a>
-        <a href="{{ route('frpTunnel.index') }}" class="mdui-list-item mdui-ripple">{{ tr('Frp Tunnel') }}</a>
+
+        <!-- Minecraft -->
+        <div>
+            <div class="mdui-list" mdui-collapse="{accordion: true}">
+                <div class="mdui-collapse-item">
+                    <div class="mdui-collapse-item-header mdui-list-item mdui-ripple">
+                        <div class="mdui-list-item-content sub-menu-title">{{ tr('Minecraft') }}</div>
+                        <i class="mdui-collapse-item-arrow mdui-icon material-icons">keyboard_arrow_down</i>
+                    </div>
+                    <div class="mdui-collapse-item-body mdui-list">
+                        <a href="{{ route('minecraftBeFlow.index') }}"
+                            class="mdui-list-item mdui-ripple">{{ tr('Bedrock Server flow') }}</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Minecraft -->
+
     </div>
 </div>
