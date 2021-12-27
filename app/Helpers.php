dc260d96604fa41a2e88b2835e872aca87508df3
iVampireSP.com <im@ivampiresp.com>
2021-12-27 17:08:42 +0800

    增加 单位换算

--- app/Helpers.php
+++ app/Helpers.php
@@ -135,3 +135,33 @@ if (!function_exists('fail')) {
         return response()->json(['status' => 0, 'data' => $data]);
     }
 }
+
+if (!function_exists('unitConversion')) {
+    function unitConversion($num)
+    {
+        $p = 0;
+        $format = 'Bytes';
+        if ($num > 0 && $num < 1024) {
+            $p = 0;
+            return number_format($num) . ' ' . $format;
+        }
+        if ($num >= 1024 && $num < pow(1024, 2)) {
+            $p = 1;
+            $format = 'KB';
+        }
+        if ($num >= pow(1024, 2) && $num < pow(1024, 3)) {
+            $p = 2;
+            $format = 'MB';
+        }
+        if ($num >= pow(1024, 3) && $num < pow(1024, 4)) {
+            $p = 3;
+            $format = 'GB';
+        }
+        if ($num >= pow(1024, 4) && $num < pow(1024, 5)) {
+            $p = 3;
+            $format = 'TB';
+        }
+        $num /= pow(1024, $p);
+        return number_format($num, 3) . ' ' . $format;
+    }
+}
