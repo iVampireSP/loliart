9513b29d2bdc1260e50e2cc71acba8ad76c0273b
iVampireSP.com <im@ivampiresp.com>
2021-12-27 10:35:51 +0800

    可填充字段

--- Modules/FrpTunnel/Entities/FrpServer.php
+++ Modules/FrpTunnel/Entities/FrpServer.php
@@ -9,10 +9,9 @@ class FrpServer extends Model
 {
     use HasFactory;
 
-    protected $fillable = [];
-    
-    protected static function newFactory()
-    {
-        return \Modules\FrpTunnel\Database\factories\FrpServerFactory::new();
-    }
+    protected $fillable = [
+        'name', 'server_address', 'server_port', 'token', 'dashboard_port', 'dashboard_user', 'dashboard_password',
+        'allow_http', 'allow_https', 'allow_tcp', 'allow_udp', 'allow_stcp', 'min_port', 'max_port', 'max_tunnels',
+        'team_id'
+    ];
 }
