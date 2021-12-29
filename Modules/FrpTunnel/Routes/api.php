193e2596768856d4ae344f8660ed186d61d2d8e7
iVampireSP.com <im@ivampiresp.com>
2021-12-29 15:43:20 +0800

    增加 api 路由

--- Modules/FrpTunnel/Routes/api.php
+++ Modules/FrpTunnel/Routes/api.php
@@ -11,8 +11,8 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
-Route::middleware('auth:api')->get('/frptunnel', function (Request $request) {
-    return $request->user();
+Route::prefix('frpTunnel')->name('api.frpTunnel.')->group(function () {
+    Route::get('/handler/{tunnel}', 'PortManagerController@handler')->name('handler');
 });
