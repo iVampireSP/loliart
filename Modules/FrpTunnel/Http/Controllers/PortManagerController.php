f74ab1bb25f6fc565559fde55befce8b4e29fd22
iVampireSP.com <im@ivampiresp.com>
2021-12-29 16:24:40 +0800

    增加 端口鉴权

---
+++ Modules/FrpTunnel/Http/Controllers/PortManagerController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Modules\FrpTunnel\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Modules\FrpTunnel\Entities\FrpServer;
+use Modules\FrpTunnel\Entities\FrpTunnel;
+
+class PortManagerController extends Controller
+{
+    public function handler(Request $request, FrpServer $server)
+    {
+        if (is_null($server)) {
+            return $this->failed('Server not found');
+        }
+
+        if ($request->op != 'NewProxy') {
+            return $this->failed('Auth failed.');
+        }
+
+        if (!is_null($request->content['user']['user'])) {
+            return $this->failed('User not allowed.');
+        }
+
+        // Search tunnel
+        $tunnel = FrpTunnel::where('client_token', $request->content['proxy_name'])->where('server_id', $server->id)->first();
+        if (is_null($tunnel)) {
+            return $this->failed('Tunnel not found.');
+        }
+
+        if ($request->content['proxy_type'] == 'tcp' || $request->content['proxy_type'] == 'udp') {
+            if ($request->content['proxy_type'] == $tunnel->proxy_type || $request->content['remote_port'] != $tunnel->remote_port || $tunnel->remote_port < 1024) {
+                return $this->failed('Tunnel not allowed.');
+            }
+        } elseif ($request->content['proxy_type'] == 'http' || $request->content['proxy_type'] == 'https') {
+            if ($request->content['proxy_type'] == $tunnel->proxy_type || $request->content['custom_domains'][0] != $tunnel->custom_domain) {
+                return $this->failed('Tunnel configuration error.');
+            }
+        }
+
+        return $this->success();
+    }
+
+    private function success()
+    {
+        $response = [
+            "reject" => false,
+            "unchange" => true,
+        ];
+
+        return response()->json($response);
+    }
+
+    private function failed($reason = null)
+    {
+        return response()->json([
+            "reject" => true,
+            "reject_reason" => $reason ?? 'Auth failed, please check your tunnel configuration or copy the configuration from ' . config('app.url'),
+            "unchange" => true,
+        ]);
+    }
+}
