a68328e720bd035a89c86241b05eaff7d2ef1371
iVampireSP.com <im@ivampiresp.com>
2021-12-29 21:51:51 +0800

    修改 菜单栏信息

--- resources/views/components/app-menu.blade.php
+++ resources/views/components/app-menu.blade.php
@@ -45,7 +45,7 @@
         </div>
 
         <div class="edge-bottom">
-            <div class="mdui-typo-caption-opacity mdui-text-center">&copy; <span class="copy-year"></span> Edge Stading. All rights reserved.
+            <div class="mdui-typo-caption-opacity mdui-text-center">Copyright <span class="copy-year"></span> &copy; EDGE STANDING LTD
             </div>
         </div>
     </div>
