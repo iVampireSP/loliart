ebcd72a5aaff5344f778f6433e5972e97b3933c4
iVampireSP.com <im@ivampiresp.com>
2022-01-05 19:57:08 +0800

    专门广播的redis

--- config/database.php
+++ config/database.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Str;
-
 return [
 
     /*
@@ -13,7 +11,7 @@ return [
     | to use as your default connection for all database work. Of course
     | you may use many connections at once using the Database library.
     |
-    */
+     */
 
     'default' => env('DB_CONNECTION', 'mysql'),
 
@@ -31,7 +29,7 @@ return [
     | so make sure you have the driver for your particular database of
     | choice installed on your machine before you begin development.
     |
-    */
+     */
 
     'connections' => [
 
@@ -105,7 +103,7 @@ return [
     | your application. Using this information, we can determine which of
     | the migrations on disk haven't actually been run in the database.
     |
-    */
+     */
 
     'migrations' => 'migrations',
 
@@ -118,7 +116,7 @@ return [
     | provides a richer body of commands than a typical key-value system
     | such as APC or Memcached. Laravel makes it easy to dig right in.
     |
-    */
+     */
 
     'redis' => [
 
@@ -135,6 +133,14 @@ return [
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
+        ],
+
+        'channel' => [
+            'url' => env('REDIS_URL'),
+            'host' => env('REDIS_CHANNEL_HOST', '127.0.0.1'),
+            'password' => env('REDIS_CHANNEL_PASSWORD', null),
+            'port' => env('REDIS_CHANNEL_PORT', '6378'),
+            'database' => env(0),
         ],
 
         'cache' => [
