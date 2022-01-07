41699631b5f1587e0329112118bfb3915e629c9e
iVampireSP <im@ivampiresp.com>
2022-01-07 13:57:49 +0000

    增加 motd 和 version

---
+++ Modules/MinecraftBEFlow/Database/Migrations/2022_01_07_215647_add_motd_to_mcbe_flow_servers_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddMotdToMcbeFlowServersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('mcbe_flow_servers', function (Blueprint $table) {
+            $table->string('motd')->nullable();
+            $table->string('version')->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('mcbe_flow_servers', function (Blueprint $table) {
+        });
+    }
+}
