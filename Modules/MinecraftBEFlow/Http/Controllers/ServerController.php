07e1e4cdefbdf92d064ee124b178f55910655f21
iVampireSP <im@ivampiresp.com>
2022-01-07 03:32:28 +0000

    添加验证

--- Modules/MinecraftBEFlow/Http/Controllers/ServerController.php
+++ Modules/MinecraftBEFlow/Http/Controllers/ServerController.php
@@ -2,9 +2,10 @@
 
 namespace Modules\MinecraftBEFlow\Http\Controllers;
 
-use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Illuminate\Contracts\Support\Renderable;
 use Modules\MinecraftBEFlow\Entities\McbeFlowServers;
 
 class ServerController extends Controller
@@ -25,7 +26,7 @@ class ServerController extends Controller
      */
     public function create()
     {
-        return view('minecraftbeflow::create');
+        return view('minecraftbeflow::servers.create');
     }
 
     /**
@@ -35,7 +36,33 @@ class ServerController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required',
+            'ip' => 'required|ip',
+            'port' => 'integer|max:65535|min:1024',
+            'start_x' => 'integer|max:10240|min:0',
+            'start_z' => 'integer|max:10240|min:0',
+            'end_x' => 'integer|max:10240|min:0',
+            'end_z' => 'integer|max:10240|min:0',
+        ]);
+
+        // 检测服务器是否存在
+        if (McbeFlowServers::where('ip', $request->ip)->where('port', $request->port)->exists()) {
+            write('Server already added.');
+            return fail();
+        }
+
+        $req = $request->toArray();
+        $req['team_id'] = session('team_id');
+        $req['token'] = Str::random(10);
+
+        McbeFlowServers::create($req);
+
+        write(route('minecraftBeFlow.servers.index'));
+
+        write('Server created.');
+
+        return success();
     }
 
     /**
