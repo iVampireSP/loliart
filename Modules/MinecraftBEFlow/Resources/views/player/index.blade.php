1689faf585d23c3c8c59c380cf7f4bd42236cb71
iVampireSP <im@ivampiresp.com>
2022-01-08 10:28:12 +0000

    增加删除

--- Modules/MinecraftBEFlow/Resources/views/player/index.blade.php
+++ Modules/MinecraftBEFlow/Resources/views/player/index.blade.php
@@ -4,11 +4,24 @@
     @include('minecraftbeflow::layouts.menu')
 @endsection
 
-@section('title', tr('Hi') . $player->name)
+@section('title', $player->name)
 
 @section('content')
     <h1 class="mdui-typo-display-1">{{ tr('Hi') }}, {{ $player->name }}</h1>
 
     <p>你加入的时间: {{ $player->created_at }}</p>
+
+    <button class="mdui-m-t-4 mdui-btn mdui-ripple mdui-btn-outlined"
+        onclick="m.delete()">{{ tr('Delete Profile') }}</button>
+
+    <script>
+        m = {
+            delete: () => {
+                ui.confirm('You cannot restore if you delete your profile, are you sure?', () => {
+                    util.delete(route('minecraftBeFlow.player.destroy'))
+                })
+            }
+        }
+    </script>
 
 @endsection
