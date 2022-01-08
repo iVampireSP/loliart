736c15b08204fda56823b9df9060346981e07846
iVampireSP <im@ivampiresp.com>
2022-01-08 14:14:46 +0000

    修复 玩家不在线时报错

--- Modules/MinecraftBEFlow/Resources/views/player/index.blade.php
+++ Modules/MinecraftBEFlow/Resources/views/player/index.blade.php
@@ -12,11 +12,16 @@
     <p>{{ tr('Joined at') }}: {{ $player->created_at }}</p>
     <p>{{ tr('Binding to server for the first time') }}: {{ $player->server->name }}</p>
 
-    @if (!is_null($cache))
-        <p>{{ tr('Position') }}: X={{ $cache['pos'][0] }} Y={{ $cache['pos'][1] }} Z={{ $cache['pos'][2] }}</p>
-    @endif
+    <div class="player-online">
+        @if (!is_null($cache))
+            <p>{{ tr('Position') }}: X={{ $cache['pos'][0] }} Y={{ $cache['pos'][1] }} Z={{ $cache['pos'][2] }}</p>
+            <p>{{ tr('Now at server') }}: <a
+                    href="{{ route('minecraftBeFlow.servers.show', $cache['server_id']) }}">{{ $cache['server_name'] }}</a>
+            </p>
+        @endif
+    </div>
 
-    <p>{{ tr('Now at server') }}: <a>{{ $cache['server_name'] }}</a></p>
+
 
     <button class="mdui-m-t-4 mdui-btn mdui-ripple mdui-btn-outlined"
         onclick="m.delete()">{{ tr('Delete Profile') }}</button>
@@ -27,7 +32,15 @@
                 ui.confirm('You cannot restore if you delete your profile, are you sure?', () => {
                     util.delete(route('minecraftBeFlow.player.destroy'))
                 })
-            }
+            },
+            // e: (type, data) {
+            //     switch (type) {
+            //         case 'mcbe.flow.player.updated':
+            //             util.reload('.player-online')
+
+            //             break;
+            //     }
+            // }
         }
     </script>
 
